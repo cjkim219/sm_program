59f107d9f38bdf239bf886fbb38fadc7fcbc5c0a
cjkim <cjkim219@gmail.com>
2023-11-01 03:33:47 +0900

    test

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("이미지 삽입 예제")
-
-photo = tk.PhotoImage(file="picture\photo_min.png")
-photo_subsample = photo.subsample(10, 10)
-label = tk.Label(root, image=photo_subsample)
-label.pack()
-
-root.mainloop()
+print("test")
